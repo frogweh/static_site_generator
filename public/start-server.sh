d3911fdf01da7d0dbabe530e5d3105438aeb307f
Frogweh <zbernath@frogweh.org>
2025-07-21 13:51:31 -0600

    Added some python

--- public/start-server.sh
+++ public/start-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cd /home/frogweh/git/workspace/github.com/frogweh/static_site_generator/public
 python3 -m http.server 8888
---
+++ src/textnode.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+class TextType(Enum):
+    text = "text"
+    bold = "bold"
+    italic = "italic"
+    code = "code"
+    link = "link"
+    image = "image"
+
+class TextNode():
+    def __init__(self, text,text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, obj):
+        if not isinstance(obj, TextNode):
+            return False
+        else:
+
+            return (
+                self.text == obj.text and
+                self.text_type == obj.text_type and
+                self.url == obj.url
+            )
+
+    def __repr__(self):
+        return (f"TextNode({self.text}, {self.text_type.value}, {self.url})")
