739273bdebf60312a9a459456048ebd3dd7ad920
Frogweh <zbernath@frogweh.org>
2025-07-21 13:55:50 -0600

    added main function in main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+#print("Hello world")
+
+from textnode import TextNode, TextType
+
+main():
+    text_node = TextNode("This is some anchor text", TextType.link, "https://www.boot.dev")
+    print(text_node)
+
+main()
