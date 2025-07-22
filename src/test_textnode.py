cba9537d37ad81d9095e6a2a3f53169a8c9502a0
frogweh <zbernath@shortbusgames.org>
2025-07-22 00:37:22 -0600

    completed the split delimiter with tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -29,6 +29,25 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a link node", TextType.link, None)
         self.assertNotEqual(node, node2)
 
+    def test_split_nodes_delim_none(self):
+        node = TextNode("This is just a plain old line of text", TextType.text)
+        new_nodes = split_nodes_delimiter([node], "", TextType.text)
+        self.assertEqual(new_nodes, [TextNode("This is just a plain old line of text", TextType.text)])
+
+    def test_split_nodes_delim_bold(self):
+        node = TextNode("This is a text with a **bolded** word", TextType.text)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.bold)
+        self.assertEqual(new_nodes, [TextNode("This is a text with a ", TextType.text), TextNode("bolded", TextType.bold), TextNode(" word", TextType.text)])
+
+    def test_split_nodes_delim_italic(self):
+        node = TextNode("This is a text with an _italicized_ word", TextType.text)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.italic)
+        self.assertEqual(new_nodes, [TextNode("This is a text with an ", TextType.text), TextNode("italicized", TextType.italic), TextNode(" word", TextType.text)])
+
+    def test_split_nodes_delim_code(self):
+        node = TextNode("This is a text with a `code block` section", TextType.text)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.code)
+        self.assertEqual(new_nodes, [TextNode("This is a text with a ", TextType.text),TextNode("code block", TextType.code), TextNode(" section", TextType.text)])
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -27,5 +27,22 @@ class TextNode():
     def __repr__(self):
         return (f"TextNode({self.text}, {self.text_type.value}, {self.url})")
 
-
-
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if not delimiter:
+        return old_nodes.copy()
+    final = []
+    for node in old_nodes:
+        if node.text_type != TextType.text:
+            final.append(node)
+            continue
+        if delimiter not in node.text:
+            final.append(node)
+            continue
+        split_text = node.text.split(delimiter)
+        if len(split_text) % 2 == 0:
+            raise Exception("Mismatched delimiter")
+        for i, chunk in enumerate(split_text):
+            type_to_use = TextType.text if i % 2 == 0 else text_type
+            if chunk:
+                final.append(TextNode(chunk, type_to_use))
+    return final
